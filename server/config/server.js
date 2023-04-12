690a8cbdd75fb2d0b38b298c0c47ca6c21691cd1
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-12 13:34:35 -0400

    first commit

--- server/config/server.js
+++ server/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://react-ecommerce-7d0j.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
