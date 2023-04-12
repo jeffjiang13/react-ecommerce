afbbaf88de7df8c2bd5eae62f0dc42c51deda803
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-12 13:12:04 -0400

    first commit

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://react-ecommerce-7d0j.onrender.com/'), // Replace with your Strapi backend URL
+  url: env('PUBLIC_URL', 'https://react-ecommerce-7d0j.onrender.com'), // Replace with your Strapi backend URL
+  app: {
+    keys: ['quhWaobvedEPBqW23JoyLA==', 'JXadhLq+9Qu/N2Er9g0ecw=='], // Add your unique keys here
+  },
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'TpEVAHJKo0jq8Wfz69g5Qg=='), // Replace with your JWT secret
+      secret: env('ADMIN_JWT_SECRET', 'g6cfxRQ+bFT8MFAuqGGUhA=='), // Replace with your JWT secret
     },
     url: '/admin',
     serveAdminPanel: env('SERVE_ADMIN_PANEL', true),
