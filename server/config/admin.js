f4b3c5637bb64f2befff0c6fa4babd003675e60a
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-24 19:06:05 -0400

    fixing backend

--- server/config/admin.js
+++ server/config/admin.js
@@ -12,5 +12,5 @@ module.exports = ({ env }) => ({
   },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_ADMIN_URL', 'https://react-ecommerce-7d0j.onrender.com'),
+  url: env('PUBLIC_URL', 'https://react-ecommerce-7d0j.onrender.com'),
 });
