db19d14b5f3a9f1d5f8b855ebed2c1db027a4aa3
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-24 18:51:50 -0400

    working on backend

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,33 +1,12 @@
 module.exports = [
-
-  "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:",'https://react-ecommerce-7d0j.onrender.com'],
-          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
-          upgradeInsecureRequests: null,
-        },
-      },
-
-    },
-    settings: {
-      cors: {
-        enabled: true,
-        origin: ['https://jj-react-ecommerce.vercel.app/'],
-      },
-    },
-  },
-  "strapi::cors",
-  "strapi::poweredBy",
-  "strapi::logger",
-  "strapi::query",
-  "strapi::body",
-  "strapi::session",
-  "strapi::favicon",
-  "strapi::public",
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
 ];
