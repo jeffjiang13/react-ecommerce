223eff42dd09628b15c5b70879da216672171898
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-25 13:45:46 -0400

    deployment

--- server/config/server.js
+++ server/config/server.js
@@ -1,10 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1335),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "8431c625cac94e7ef8143372f074f507"),
+    },
   },
 });
