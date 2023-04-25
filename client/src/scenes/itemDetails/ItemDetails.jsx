6e15cb63319bf955cea8c8a1682b2b1cb2314263
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-25 14:23:30 -0400

    deployment

--- client/src/scenes/itemDetails/ItemDetails.jsx
+++ client/src/scenes/itemDetails/ItemDetails.jsx
@@ -25,7 +25,7 @@ const ItemDetails = () => {
 
   async function getItem() {
     const item = await fetch(
-      `https://react-ecommerce-7d0j.onrender.com/api/items/${itemId}`,
+      `https://react-ecommerce-7d0j.onrender.com/items/${itemId}`,
       {
         method: "GET",
       }
@@ -36,7 +36,7 @@ const ItemDetails = () => {
 
   async function getItems() {
     const items = await fetch(
-      `https://react-ecommerce-7d0j.onrender.com/api/items`,
+      `https://react-ecommerce-7d0j.onrender.com/items`,
       {
         method: "GET",
       }
