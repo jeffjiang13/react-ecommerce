89eb2550f3d7eff2c2fcad0b721b73ab945d5b6d
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-24 21:06:39 -0400

    fixing backend

--- server/config/server.js
+++ server/config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://react-ecommerce-7d0j.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
