67fc5fc283c8e883f0064692f3d6c16cf84aa2f5
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-25 23:12:37 -0400

    deployment

--- server/config/middlewares.js
+++ server/config/middlewares.js
@@ -1,15 +1,12 @@
-module.exports = {
-  load: {
-    before: ['responseTime', 'logger', 'cors', 'responses', 'gzip'],
-    order: [
-      "Define the middlewares' load order by putting their name in this array is the right order",
-    ],
-    after: ['parser', 'router'],
-  },
-  settings: {
-    cors: {
-      enabled: true,
-      origin: '*',
-    },
-  },
-};
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
--- server/config/server.js
+++ server/config/server.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1335),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "8431c625cac94e7ef8143372f074f507"),
-    },
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
   },
 });
