b26832fcbf4b3b358c774ad2d8e4674a7161846e
Jeff Jiang <jeff.jiang13@gmail.com>
2023-04-28 17:47:26 -0400

    working

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -36,8 +36,8 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         payment_method_types: ["card"],
         customer_email: email,
         mode: "payment",
-        success_url: "http://localhost:2001/checkout/success",
-        cancel_url: "http://localhost:2001",
+        success_url: "https://jj-react-ecommerce.vercel.app/checkout/success",
+        cancel_url: "https://jj-react-ecommerce.vercel.app/",
         line_items: lineItems,
       });
 
